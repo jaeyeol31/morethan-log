2cadc4095c7e8891061db3962090adeb56b2a8cf
yeoduf <woduf31@gmail.com>
2024-08-06 19:15:42 +0900

    11

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -47,20 +47,7 @@ const ContactCard: React.FC = () => {
             <div className="name">email</div>
           </a>
         )}
-        {CONFIG.profile.linkedin && (
-          <a
-            href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiFillLinkedin className="icon" />
-            <div className="name">linkedin</div>
-          </a>
-        )}
-      </StyledWrapper>
-    </>
-  )
-}
+       
 
 export default ContactCard
 
