113cfd82cf9e243553cca4eedf6b33d4ea3cda27
재열31 <160302517+jaeyeol31@users.noreply.github.com>
2024-08-06 19:34:37 +0900

    Update ContactCard.tsx

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -47,7 +47,20 @@ const ContactCard: React.FC = () => {
             <div className="name">email</div>
           </a>
         )}
-       
+        {CONFIG.profile.linkedin && (  // 링크드인 정보 추가
+          <a
+            href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiFillLinkedin className="icon" />
+            <div className="name">linkedin</div>
+          </a>
+        )}
+      </StyledWrapper>
+    </>
+  )
+}
 
 export default ContactCard
 
